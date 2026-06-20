926eca3fa8d148741929ce77b42b092f92b5c444
kevnml <kevnml@evaluacion.local>
2026-06-20 12:46:38 -0600

    test: agregando prueba loca del gato xd

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayName = document.getElementById('display-name');
     const displayId = document.getElementById('display-id');
     const displayDate = document.getElementById('display-date');
+    const displayCat = document.getElementById('display-cat');
     
     const inputName = document.getElementById('input-name');
     const inputId = document.getElementById('input-id');
+    const inputCat = document.getElementById('input-cat');
     const updateForm = document.getElementById('update-form');
 
     // Set today's date
@@ -23,12 +25,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const loadData = () => {
         const savedName = localStorage.getItem('studentName') || defaultName;
         const savedId = localStorage.getItem('studentId') || defaultId;
+        const savedCat = localStorage.getItem('studentCat') || "Ingresa un gato";
         
         displayName.textContent = savedName;
         displayId.textContent = savedId;
+        displayCat.textContent = savedCat;
         
         inputName.value = savedName;
         inputId.value = savedId;
+        inputCat.value = savedCat === "Ingresa un gato" ? "" : savedCat;
     };
 
     // Update data
@@ -37,15 +42,24 @@ document.addEventListener('DOMContentLoaded', () => {
         
         const newName = inputName.value.trim();
         const newId = inputId.value.trim();
+        const newCat = inputCat.value.trim();
         
-        if (newName && newId) {
+        if (newName && newId && newCat) {
             // Save to localStorage (Local DB alternative)
             localStorage.setItem('studentName', newName);
             localStorage.setItem('studentId', newId);
+            localStorage.setItem('studentCat', newCat);
             
             // Update display
             displayName.textContent = newName;
             displayId.textContent = newId;
+            displayCat.textContent = newCat;
+
+            // SCROLL FEATURE
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
 
             // Visual feedback
             const card = document.querySelector('.glass');
@@ -69,6 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const resetData = () => {
         localStorage.removeItem('studentName');
         localStorage.removeItem('studentId');
+        localStorage.removeItem('studentCat');
         loadData();
 
         // Visual feedback
